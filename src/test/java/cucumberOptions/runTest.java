56dd3e59a31e1fe2c595fa3131a33fcfe9479085
Chandrakant Bhirud <chandubhirud@yahoo.com>
2022-03-02 17:38:20 +0000

    Cucmber report added

--- src/test/java/cucumberOptions/runTest.java
+++ src/test/java/cucumberOptions/runTest.java
@@ -8,7 +8,8 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(features= "src/test/java/features",
 		glue= "stepDefinations", 
-		tags="@SmokeTest")	
+		tags="@SmokeTest",
+		monochrome = true,plugin = {"html:target/cucumber-html-report", "json:target/cucumber-json-report.json" })	
 public class runTest {
 
 }
